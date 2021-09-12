956c06342aad788e414acb3e856c3bc5eea98c23
supriyo mondal <hisupriyooo@gmail.com>
2021-09-12 14:57:44 +0530

    complete header

--- src/components/Header/TeamHeader.js
+++ src/components/Header/TeamHeader.js
@@ -6,56 +6,54 @@ import {colors} from '../../Styles/colors';
 import commonStyles from '../../Styles/commonStyles';
 import {sizing} from '../../Styles/theme';
 import {Shadow} from 'react-native-neomorph-shadows';
+import WalletBalance from '../Wallet/WalletBalance';
 
 const TeamHeader = () => {
-	return (
-		<ElHeader
-			leftComponent={
-				<View>
-					<View style={commonStyles.rowAlignCenterJustifyBetween}>
-						<Text style={styles.teamName}>GER</Text>
-						<Text style={{color: colors.primary}}>vs</Text>
-						<Text style={styles.teamName}>ENG</Text>
-					</View>
-					<View style={commonStyles.rowAlignCenterJustifyBetween}>
-						{['00', '14', '32'].map((item, i) => (
-							<Shadow inner key={i.toString()} style={styles.timer}>
-								<Text style={styles.timerText}>{item}</Text>
-							</Shadow>
-						))}
-					</View>
-				</View>
-			}
-			rightComponent={{
-				text: 'Wallet',
-				style: {...Typography.h3Style, color: '#fff'},
-			}}
-			backgroundColor={colors.backgroundColor}
-			containerStyle={{elevation: 3, marginHorizontal: sizing.x8}}
-		/>
-	);
+  return (
+    <ElHeader
+      leftComponent={
+        <View>
+          <View style={commonStyles.rowAlignCenterJustifyBetween}>
+            <Text style={styles.teamName}>GER</Text>
+            <Text style={{color: colors.primary}}>vs</Text>
+            <Text style={styles.teamName}>ENG</Text>
+          </View>
+          <View style={commonStyles.rowAlignCenterJustifyBetween}>
+            {['00', '14', '32'].map((item, i) => (
+              <Shadow inner key={i.toString()} style={styles.timer}>
+                <Text style={styles.timerText}>{item}</Text>
+              </Shadow>
+            ))}
+          </View>
+        </View>
+      }
+      rightComponent={() => <WalletBalance />}
+      backgroundColor={colors.backgroundColor}
+      containerStyle={{elevation: 3, borderBottomWidth: 0}}
+    />
+  );
 };
 
 export default TeamHeader;
 
 const styles = StyleSheet.create({
-	teamName: {
-		margin: sizing.x4,
-		...Typography.h3Style,
-		color: colors.white,
-		fontWeight: 'bold',
-	},
-	timer: {
-		height: 28,
-		width: 28,
-		justifyContent: 'center',
-		alignItems: 'center',
-		shadowRadius: 2.5,
-		borderRadius: 4,
-		marginVertical: 3,
-	},
-	timerText: {
-		color: colors.secondaryColor,
-		fontSize: 16,
-	},
+  teamName: {
+    margin: sizing.x4,
+    ...Typography.h3Style,
+    color: colors.white,
+    fontWeight: 'bold',
+  },
+  timer: {
+    height: 28,
+    width: 28,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowRadius: 2.5,
+    borderRadius: 4,
+    marginVertical: 3,
+  },
+  timerText: {
+    color: colors.secondaryColor,
+    fontSize: 16,
+  },
 });
